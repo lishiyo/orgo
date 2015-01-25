1e2c90c8e121e5759cc35fb8663807c00af69c18
lishiyo <c.ziye.li@gmail.com>
2015-01-25 08:06:07 +0000

    updates web.js for grunt

--- web.js
+++ web.js
@@ -4,13 +4,5 @@ var morgan = require('morgan');
 var app = express();
  
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname));
-app.listen(process.env.PORT || 3000);
-
-// var gzippo = require('gzippo');
-// var express = require('express');
-// var app = express();
- 
-// app.use(express.logger('dev'));
-// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-// app.listen(process.env.PORT || 5000);
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
